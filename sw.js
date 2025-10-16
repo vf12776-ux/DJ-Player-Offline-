0c7f1b3b5ca78e168e5a7f3d2b28f8daed002d70
vf12776-ux <vf12776@gmail.com>
2025-10-16 22:48:43 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'dj-player-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
