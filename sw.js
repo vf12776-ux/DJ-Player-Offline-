e714979d917b82d952ab8881291d28e9f2bbd568
vf12776-ux <vf12776@gmail.com>
2025-10-17 00:18:09 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
 
+const CACHE_NAME = 'dj-player-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://unpkg.com/react@17/umd/react.development.js',
+  'https://unpkg.com/react-dom@17/umd/react-dom.development.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
